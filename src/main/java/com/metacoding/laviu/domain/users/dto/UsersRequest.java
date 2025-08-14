f643682e053bf3282d6284f5182544a884308bf9
verynahce <kims1577@naver.com>
2025-08-14 17:33:31 +0900

    [feat] 유저 검색, 방송 해시태그 검색 (#16)

--- src/main/java/com/metacoding/laviu/domain/users/dto/UsersRequest.java
+++ src/main/java/com/metacoding/laviu/domain/users/dto/UsersRequest.java
@@ -1,4 +1,11 @@
 package com.metacoding.laviu.domain.users.dto;
 
+import lombok.Data;
+
 public class UsersRequest {
+
+    @Data
+    public static class SearchDTO {
+        private String query;
+    }
 }
