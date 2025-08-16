16dd814db12dae9c7292e3414ad49c89f3a9a294
verynahce <kims1577@naver.com>
2025-08-16 22:11:20 +0900

    [feat] 방송 신고 (#17)

--- src/main/java/com/metacoding/laviu/domain/abusereports/dto/AbuseReportsRequest.java
+++ src/main/java/com/metacoding/laviu/domain/abusereports/dto/AbuseReportsRequest.java
@@ -1,4 +1,14 @@
 package com.metacoding.laviu.domain.abusereports.dto;
 
+import lombok.Data;
+
 public class AbuseReportsRequest {
+
+    @Data
+    public static class saveDTO {
+
+        private Integer categoryId;
+        private String details;
+
+    }
 }
