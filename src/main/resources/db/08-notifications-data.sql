78b62ec19d39fbfeec5d994f59bdbf2dce122ac2
Sxias <a32176740@gmail.com>
2025-08-20 17:18:45 +0900

    [fix] db 수정

--- src/main/resources/db/08-notifications-data.sql
+++ src/main/resources/db/08-notifications-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO notifications_tb (related_entity_id, content, is_read, type, user_id, created_at)
 VALUES
-    -- stream_id가 1,2,3인 방송알림을 user_id 2인 사용자에게 보냄
+    -- stream_id로 방송알림을 user_id 2인 사용자에게 보냄
     (1, 'ssar님의 방송이 시작되었습니다. 시청하러 가볼까요?', FALSE, 'LIVE_STARTED', 2, NOW()),
     (3, 'love님의 방송이 시작되었습니다. 시청하러 가볼까요?', FALSE, 'LIVE_STARTED', 2, NOW()),
     (4, 'hate님의 방송이 시작되었습니다. 시청하러 가볼까요?', TRUE, 'LIVE_STARTED', 2, NOW());
